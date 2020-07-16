10f656193dc4e207769af0e6f865038ece2505f8
zazitufu <57139031+zazitufu@users.noreply.github.com>
2020-07-16 14:25:14 +0800

    Create omz.sh
    1、安装zsh
    2、安装ohmyzsh
    3、安装两个插件
    4、将插件添加到配置文件
    5、加/aabin到环境变量PATH

---
+++ omz.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+### 1、安装zsh 
+apt-get -y install zsh curl iftop mtr git lrzsz wget dos2unix zip unzip
+### 2、安装完毕以后，你需要将zsh替换为你的默认shell,输入下面命令进行替换:
+chsh -s /bin/zsh
+### 3、安装Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+### 4、安装插件zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+### 5、安装插件zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+### 6、修改本用户~/.zshrc
+sed -i 's/plugins=(git)/plugins=(\ngit\nzsh-autosuggestions\nzsh-syntax-highlighting\n)/' ~/.zshrc
+### 7、添加路径/aabin
+echo \'export PATH=\"\$PATH:/aabin\"\' >> ~/.zshrc
+### 8、重启zsh
+source ~/.zshrc
