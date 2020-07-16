6ed2c2c43dc48a5396d7fcc7b23b9d5be9014844
zazitufu <57139031+zazitufu@users.noreply.github.com>
2020-07-16 15:05:07 +0800

    Create omz2.sh

---
+++ omz2.sh
@@ -0,0 +1,10 @@
+### 4、安装插件zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+### 5、安装插件zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+### 6、修改本用户~/.zshrc
+sed -i 's/plugins=(git)/plugins=(\ngit\nzsh-autosuggestions\nzsh-syntax-highlighting\n)/' ~/.zshrc
+### 7、添加路径/aabin
+echo \'export PATH=\"\$PATH:/aabin\"\' >> ~/.zshrc
+### 8、重启zsh
+source ~/.zshrc
