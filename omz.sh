8e8ae71aa79cb4b482f0f01e6bce2548436666c2
zazitufu <57139031+zazitufu@users.noreply.github.com>
2021-06-03 00:06:55 +0800

    Update omz.sh

--- omz.sh
+++ omz.sh
@@ -2,17 +2,17 @@
 ### 1、安装zsh 
 apt-get update
 apt-get -y install zsh git wget
-### 2、安装完毕以后，你需要将zsh替换为你的默认shell,输入下面命令进行替换:
-chsh -s /bin/zsh
-### 3、安装Oh My Zsh
+### 2、安装Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-### 4、安装插件zsh-autosuggestions
+### 3、安装插件zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-### 5、安装插件zsh-syntax-highlighting
+### 4、安装插件zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-### 6、修改本用户~/.zshrc
+### 5、修改本用户~/.zshrc
 sed -i 's/plugins=(git)/plugins=(\ngit\nzsh-autosuggestions\nzsh-syntax-highlighting\n)/' ~/.zshrc
-### 7、添加路径/aabin
+### 6、添加路径/aabin
 echo \'export PATH=\"\$PATH:/aabin\"\' >> ~/.zshrc
+### 7、安装完毕以后，你需要将zsh替换为你的默认shell,输入下面命令进行替换:
+chsh -s /bin/zsh
 ### 8、重启zsh
 source ~/.zshrc
