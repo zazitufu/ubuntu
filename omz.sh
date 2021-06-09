f15d6c3f2a14da125949cd821461c4d84c770683
zazitufu <57139031+zazitufu@users.noreply.github.com>
2021-06-09 09:51:09 +0800

    Update omz.sh

--- omz.sh
+++ omz.sh
@@ -13,9 +13,9 @@ sed -i 's/plugins=(git)/plugins=(\ngit\nzsh-autosuggestions\nzsh-syntax-highligh
 ### 6 建立两个目录
 mkdir /aalog && mkdir /aabin
 ### 7、添加路径/aabin
-echo \'export PATH=\$PATH:/aabin\' >> ~/.zshrc
-echo \'alias cdl=\"cd /aalog\"\'  >> ~/.zshrc
-echo \'alias cdb=\"cd /aabin\"\'  >> ~/.zshrc
+echo export PATH=\$PATH:/aabin >> ~/.zshrc
+echo alias cdl=\"cd /aalog\"  >> ~/.zshrc
+echo alias cdb=\"cd /aabin\"  >> ~/.zshrc
 ### 8、安装完毕以后，你需要将zsh替换为你的默认shell,输入下面命令进行替换:
 chsh -s /bin/zsh
 ### 9、重启zsh
