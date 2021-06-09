55bf7955d19b9e3d3197c25ccf9fbab8b136ab38
zazitufu <57139031+zazitufu@users.noreply.github.com>
2021-06-09 08:39:49 +0800

    Update omz.sh

--- omz.sh
+++ omz.sh
@@ -11,9 +11,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 ### 5、修改本用户~/.zshrc
 sed -i 's/plugins=(git)/plugins=(\ngit\nzsh-autosuggestions\nzsh-syntax-highlighting\n)/' ~/.zshrc
 ### 6 建立两个目录
-mkdir /aalog && mkdir /aabin && echo \'export PATH=\"\$PATH:/aabin\"\' >> /etc/profile
+mkdir /aalog && mkdir /aabin
 ### 7、添加路径/aabin
-echo \'export PATH=\"\$PATH:/aabin\"\' >> ~/.zshrc
+echo \'export PATH=\$PATH:/aabin\' >> ~/.zshrc
 ### 8、安装完毕以后，你需要将zsh替换为你的默认shell,输入下面命令进行替换:
 chsh -s /bin/zsh
 ### 9、重启zsh
