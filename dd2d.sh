e1b27d2d30bab9287906e5f7314d780ece57b0ba
zazitufu <57139031+zazitufu@users.noreply.github.com>
2021-06-10 21:24:58 +0800

    Create dd2d.sh

---
+++ dd2d.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+### 本脚本用来快速DD为Debian10，默认为使用全球CDN的镜像 deb.debian.org
+### 使用其他镜像可在此网址搜寻：https://www.debian.org/mirror/list
+### 香港站为：http://ftp.hk.debian.org/debian/
+### 默认root密码为Test233，首次登录后修改。
+apt-get update
+apt-get install -y xz-utils openssl gawk file wget 
+wget https://raw.githubusercontent.com/zazitufu/ubuntu/master/dd.sh
+chmod a+x dd.sh
+bash dd.sh -d 10 -v 64 -a --mirror 'deb.debian.org/debian/' -p Test233
