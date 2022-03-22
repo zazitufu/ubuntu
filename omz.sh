a0f86f668dcac7d30d19aa3ee632ae09f62a3a39
zazitufu <57139031+zazitufu@users.noreply.github.com>
2022-03-22 15:04:41 +0800

    Update omz.sh
    1，zsh的路径修正
    2，使用Here Document 的方式，将多行环境变量进行添加。不再用echo 方式添加。

--- omz.sh
+++ omz.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+##### version 0.3
+##### 2022年3月22日15点04分
 ##### 运行完后reboot一下系统。使用方法:
 ##### bash <(curl -s -L https://github.com/zazitufu/ubuntu/raw/master/omz.sh)
+##### bash <(curl -sL https://git.io/zaziomz)
+### 0、先安装curl
+apt-get -y install curl
+
 ### 1、安装zsh 
 apt-get update
 apt-get -y install zsh git wget
@@ -13,14 +19,40 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 ### 5、修改本用户~/.zshrc，包括主题改为agnoster，添加插件
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
 sed -i 's/plugins=(git)/plugins=(\ngit\nzsh-autosuggestions\nzsh-syntax-highlighting\n)/' ~/.zshrc
+
 ### 6 建立两个目录
 mkdir /aalog && mkdir /aabin
-### 7、添加路径/aabin
-echo export PATH=\$PATH:/aabin >> ~/.zshrc
-echo alias cdl=\"cd /aalog\"  >> ~/.zshrc
-echo alias cdb=\"cd /aabin\"  >> ~/.zshrc
-echo alias scp=\'noglob scp\' >> ~/.zshrc
+
+### 7、添加下面整段环境变量到 ~/.zshrc
+cat << "EOF" >> ~/.zshrc
+export PATH=$PATH:/aabin
+alias cdl="cd /aalog"
+alias cdb="cd /aabin"
+alias gip="curl -s https://api.myip.la"
+# 会影响scp 对路径中的通配符进行展开，先取消 echo alias scp=\'noglob scp\' >> ~/.zshrc
+
+## Automatically quote globs in URL and remote references ，解决zsh下的通配符*的展开故障。
+__remote_commands=(scp rsync)
+autoload -U url-quote-magic
+zle -N self-insert url-quote-magic
+zstyle -e :urlglobber url-other-schema '[[ $__remote_commands[(i)$words[1]] -le ${#__remote_commands} ]] && reply=("*") || reply=(http https ftp)'
+
+## 代理服务器
+# set a local proxy
+function proxyl() {
+    export ALL_PROXY="socks5h://127.0.0.1:port"
+}
+# set a remote proxy
+function proxyr() {
+    export ALL_PROXY="socks5h://tufu:password@ip:port"
+}
+# unset proxy
+function noproxy() {
+    unset ALL_PROXY
+}
+EOF
+
 ### 8、安装完毕以后，你需要将zsh替换为你的默认shell,输入下面命令进行替换:
-chsh -s /bin/zsh
+chsh -s /usr/bin/zsh
 ### 9、重启zsh
 source ~/.zshrc
